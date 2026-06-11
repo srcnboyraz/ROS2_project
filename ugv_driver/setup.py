e4057d51015f9be1d44aa0f3594126ed3691ee44
srcnboyraz <srcnboyraz@gmail.com>
2026-06-11 16:36:14 +0300

    Add ugv_driver ROS 2 package

---
+++ ugv_driver/setup.py
@@ -0,0 +1,33 @@
+from setuptools import find_packages, setup
+import os
+from glob import glob
+
+package_name = 'ugv_driver'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='seso',
+    maintainer_email='seso@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+          'motor_driver_node = ugv_driver.motor_driver_node:main'
+        ],
+    },
+)
